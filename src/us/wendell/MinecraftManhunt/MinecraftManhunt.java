7365adcb1b2e65da6027c33ec00c02e0019e543a
Wending Wu <wending.wu.23@dartmouth.edu>
2020-12-09 22:55:48 -0500

    add TODOs

--- src/us/wendell/MinecraftManhunt/MinecraftManhunt.java
+++ src/us/wendell/MinecraftManhunt/MinecraftManhunt.java
@@ -5,13 +5,17 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashSet;
 
-public class MinecraftManhunt extends JavaPlugin {
+public class MinecraftManhunt extends JavaPlugin implements Listener {
 
+    //TODO: Handle hunter death -- give new compass upon respawn when the game is started and remove any compasses
+    //that they drop
+    //TODO: look at Listener tutorial: https://www.youtube.com/watch?v=a9X0EeXtomY&list=PL65-DKRLvp3Yn7iglPfxKoc7bl0N80XgG&index=5
     HashSet<Player> hunters, runners;
 
     /**
@@ -40,6 +44,7 @@ public class MinecraftManhunt extends JavaPlugin {
      */
     public boolean onCommand(){
         //TODO: implement command handling
+        //when adding hunters, give them a compass
         return true;
     }
 
