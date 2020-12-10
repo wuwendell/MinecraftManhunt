f3dc548472890e8f900b37f1939deca19212dceb
Wending Wu <wending.wu.23@dartmouth.edu>
2020-12-09 22:49:02 -0500

    Backend features; commands still wip

--- README.md
+++ README.md
@@ -1,18 +1,27 @@
-## MyFirstPlugin
+## MinecraftManhunt
 
-A location for me to practice developing spigot plugins. Just for fun!
+A copy of features which are found in Dream's manhunt videos. 
+The plugin is just for fun and mostly for practice/getting used to 
+spigot api features!
 
 #### Compilation
 
-I'm using IntelliJ to compile my classes into the jar located in the out folder.
+I'm using IntelliJ to compile my classes into the jar located in the out folder. 
+One day, I will include the actual javac command used to compile everything.
 
 #### Usage
 
-Drop the MyFirstPlugin.jar file into your plugins folder before you start up a spigot server.
+Drop the MinecratManhunt.jar file into your plugins folder before you start up a spigot server.
+
+###### Commands:
+
+1. `/hunters [add|remove|clear] {username}`
+2. `/runners [add|remove|clear] {username}`
+3. `/start`
 
 #### File Structure
  - `src`: location of the source files in their respective packages
-   - `us.wendell.MyFirstPlugin` is home to the plugin's main functionality like startup 
-     and shutdown and command handling
+   - `us.wendell.MinecraftManhunt` is home to the plugin's main functionality like startup,
+     shutdown, and command handling
  - `out`: location of the compiled jar which you can drop into your plugins folder
  - `build.xml`: includes a target for copying plugin jar to my local test server
---
+++ src/us/wendell/MinecraftManhunt/MinecraftManhunt.java
@@ -0,0 +1,88 @@
+package us.wendell.MinecraftManhunt;
+
+import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashSet;
+
+public class MinecraftManhunt extends JavaPlugin {
+
+    HashSet<Player> hunters, runners;
+
+    /**
+     * Setup everything upon startup, restart, or reload.
+     */
+    @Override
+    public void onEnable(){
+        hunters = new HashSet<>();
+        runners = new HashSet<>();
+    }
+
+    /**
+     * Teardown everything upon shutdown, restart, or reload.
+     */
+    @Override
+    public void onDisable(){
+        hunters = null;
+        runners = null;
+    }
+
+    /**
+     * Implements:
+     * - /hunters [add|remove|clear] {username}
+     * - /runners [add|remove|clear] {username}
+     * - /start
+     */
+    public boolean onCommand(){
+        //TODO: implement command handling
+        return true;
+    }
+
+    /**
+     * Handle a click by any player.
+     * @param event the click event
+     */
+    @EventHandler()
+    public void onClick(PlayerInteractEvent event){
+        Player hunter = event.getPlayer();
+        //if the player is a hunter and they clicked a compass
+        if(hunters.contains(hunter) && event.getItem().getType() == Material.COMPASS){
+            //try to get the closest player
+            Player closestRunner = closestRunnerToLocation(hunter.getLocation());
+            if(closestRunner != null){
+                hunter.setCompassTarget(closestRunner.getLocation());
+                hunter.sendMessage(Color.GREEN + "Now tracking " + closestRunner.getName() + "!");
+            }else{
+                hunter.sendMessage(Color.RED + "There are no runners to track!");
+            }
+        }
+    }
+
+    /**
+     * Given a location, determine the closest runner to the location if there is one
+     * in the same world as the given location. Otherwise, return null;
+     * @param hunterLocation the given location (presumably of a hunter)
+     * @return the closest player in runners who is in the same world as location, otherwise null
+     */
+    private Player closestRunnerToLocation(Location hunterLocation){
+        Player closest = null;
+        double closestDistanceSquared = Double.MAX_VALUE;
+        for(Player p : runners){
+            try{
+                // if the player's distance squared is closer than the current closest distance squared
+                if(hunterLocation.distanceSquared(p.getLocation()) < closestDistanceSquared){
+                    closest = p;
+                    closestDistanceSquared = hunterLocation.distanceSquared(p.getLocation());
+                }
+            }catch(IllegalArgumentException e){ // they were not in the same world
+                //don't do anything
+            }
+        }
+        return closest;
+    }
+}
